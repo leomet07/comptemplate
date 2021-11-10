afb99caec90c7e0ff0be0b6f1df5125c662f231d
leomet07 <leomet07@gmail.com>
2021-11-09 20:46:58 -0500

    Basic func

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main() {
     
+    int a[] = {0, 1 , 1, 0 , 1};
+    int n = sizeof(a) / sizeof(int);
+    int pre[n + 1];
+    pre[0] = 0;
 
-	cout << "Hello world" << endl;
+    for (int i = 1; i < n + 1; i++){
+      pre[i] = pre[i -1] + a[i-1];
+    }
+
+    for (int i = 0; i < sizeof(pre) / sizeof(int); i++){
+        int curr = pre[i];
+        cout << i << ": " << curr << endl;
+    }
+	cout << "Prefix sums" << endl;
     return 0;
 }
