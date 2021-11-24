7a1869f7c39640438fcf9ef2c8861c12d3af7295
leomet07 <leomet07@gmail.com>
2021-11-24 14:02:53 -0500

    Back to template

--- main.cpp
+++ main.cpp
@@ -3,18 +3,8 @@
 using namespace std;
  
 int main() {
-    string i;
-    cin >> i;
     
-    long long w = stol(i);
-    
-    if (w % 2 == 0 && w != 2){
-        cout << "YES" << endl;
-        
-    } else{
-        cout << "NO" <<endl;
-    }
-    
+    cout << "Hello world" << endl;    
     return 0;
 
 }
