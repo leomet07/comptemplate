6c3f2868a7dfc8b2bf9d7ca880a6345b97d19856
leomet07 <leomet07@gmail.com>
2021-11-25 14:26:07 -0500

    competitive optimizations

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
  
 using namespace std;
  
+int getInt(){
+    int i;
+    cin >> i;
+    return i;
+}
+
+long long getLongLong(){
+    long long l;
+    cin >> l;
+    return l;
+}
+
+
 int main() {
-    
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
     cout << "Hello world" << endl;    
     return 0;
 
