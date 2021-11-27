fc3f7ef7645a02b86982fc05d9abf9d862cd7cfa
leomet07 <leomet07@gmail.com>
2021-11-27 09:11:58 -0500

    added getString

--- main.cpp
+++ main.cpp
@@ -14,12 +14,17 @@ long long getLongLong(){
     return l;
 }
 
+string getString(){
+    string s;
+    cin >> s;
+    return s;
+}
 
 int main() {
     ios::sync_with_stdio(0);
 	cin.tie(0);
 
-    cout << "Hello world" << endl;    
+    cout << "Comp Template" << endl;    
     return 0;
 
 }
