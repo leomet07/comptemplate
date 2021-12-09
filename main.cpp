7526c01d703ca6d892b6e82bb08f0c44bc71c649
leomet07 <leomet07@gmail.com>
2021-12-09 10:06:53 -0500

    Added data structures, ll shortcut

--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
- 
+#include <bits/stdc++.h>
 using namespace std;
- 
-int getInt(){
-    int i;
-    cin >> i;
-    return i;
-}
 
-long long getLongLong(){
-    long long l;
-    cin >> l;
-    return l;
-}
-
-string getString(){
-    string s;
-    cin >> s;
-    return s;
-}
-
+#define ll long long
 int main() {
     ios::sync_with_stdio(0);
 	cin.tie(0);
-
+    
     cout << "Comp Template" << endl;    
     return 0;
 
